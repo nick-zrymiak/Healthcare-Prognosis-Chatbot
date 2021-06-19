365b0e685f15ac3a529c0d743461fafa3bf404d5
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-06-19 23:48:54 +0600

    add react app for frontend named client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Healthcare-Analyser-and-Chatbot
+        </p> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
