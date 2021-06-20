d1dc83adf4781e3e59ce52870f01dd50e7e61fbc
Adam Liang <50341948+adamliang2019@users.noreply.github.com>
2021-06-20 00:00:39 -0700

    firebase and user signup

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 
 ReactDOM.render(
-  <App />, 
-  document.getElementById("root")
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
