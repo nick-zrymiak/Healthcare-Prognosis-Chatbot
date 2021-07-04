72d4c6ee55b8f21a25744ca7d6a00adacf7d5372
awshaf <ahmmadawshaf@gmail.com>
2021-07-04 20:14:30 +0600

    authButton changes according to logged in or not

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,41 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
-import './LandingPage.css';
+import "./LandingPage.css";
 import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-
 export default function NavBar() {
-    const[refresh,setRefresh] = useState(false);
-    const history = useHistory();
-    const [error, setError] = useState("");
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-    const { currentUser, logout } = useAuth();
+  const { currentUser, logout } = useAuth();
+  const [refresh, setRefresh] = useState(false);
+  const [error, setError] = useState("");
+  const history = useHistory();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => checkIfLoggedIn(), []);
-    
-    const checkIfLoggedIn = () => {
-        console.log('current user', currentUser);
-        if( currentUser !=null){
-            setIsLoggedIn(true);
-        }
-        else
-            setIsLoggedIn(false);
+  useEffect(() => checkIfLoggedIn(), [isLoggedIn]);
+
+  const checkIfLoggedIn = () => {
+    // renderAuthButton();
+    console.log("isLoggedIn:", isLoggedIn);
+    console.log("current user", currentUser);
+    if (currentUser != null) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
     }
+  };
 
-    const handleLogout = () => {
-        console.log('logging out');
-        console.log('current user now:', currentUser)
-        setError("");
-        try {
-          logout();
-        //   history.push("/login");
-            setRefresh(!refresh);
-        } catch {
-          setError("Failed to log out");
-        }
-      }
-    
+  const handleLogout = () => {
+    console.log("logging out...");
+    setError("");
+    try {
+      logout();
+      history.push("/landingPage");
+      setRefresh(!refresh);
+    } catch {
+      setError("Failed to log out");
+    }
+    checkIfLoggedIn();
+    history.push("/landingPage");
+    console.log("current user now:", currentUser);
+  };
+
   return (
     <div>
       <>
@@ -43,16 +46,31 @@ export default function NavBar() {
           <div className="brandName">
             <Navbar.Brand href="#home">HealthCare</Navbar.Brand>
           </div>
-        <Nav className="navbarLinks mr-auto">
+          <Nav className="navbarLinks mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#purpose">Purpose</Nav.Link>
             <Nav.Link href="#apply">Apply</Nav.Link>
 
-            {isLoggedIn
-                ?<Button variant="primary" onClick={()=>{handleLogout()}}>LogOut</Button>
-                :<Button variant="primary" onClick={()=>{history.push("/login")}}>Login</Button>
-            }
-        </Nav>
+            {isLoggedIn ? (
+              <Button
+                variant="primary"
+                onClick={() => {
+                  handleLogout();
+                }}
+              >
+                LogOut
+              </Button>
+            ) : (
+              <Button
+                variant="primary"
+                onClick={() => {
+                  history.push("/login");
+                }}
+              >
+                Login
+              </Button>
+            )}
+          </Nav>
         </Navbar>
       </>
     </div>
