4634dd3f65c8bd4208d7298e1aa577acc6c780b0
awshaf <ahmmadawshaf@gmail.com>
2021-07-20 02:37:31 +0600

    added first and last name forms

--- client/src/components/UserDataRegister.js
+++ client/src/components/UserDataRegister.js
@@ -1,49 +1,45 @@
 import React, { useState } from "react";
-import { Button, Form, Container, Row } from "react-bootstrap";
+import { Button, Form, Container, Row , Col} from "react-bootstrap";
 
 export default function UserDataRegister() {
-
-    const [firstName, setFirstName] = useState('');
-
-  const [user, setUser] = useState({
-    firstName: "Awshaf",
-    lastName: "Ishtiaque",
-    telephone: "33333333",
-    age: 28,
-    email: "test@gmail.com",
-    state: "xxxxx",
-    country: "adafafafa",
-    address: "Home",
-    address1: "adsfasdfasd adsf sad f",
-    address2: "ads fasd fasdf asdf ",
-    interests: [],
-    subscribenewsletter: false,
-  });
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log('clicked on submit');
-      console.log(firstName);
-    //   console.log(firstName);
-  }
+    e.preventDefault();
+    console.log("clicked on submit");
+    console.log(firstName);
+    console.log(lastName)
+  };
 
   return (
-    <div  onSubmit={handleSubmit} className="w-75">
-      <Form >
-          <Form.Label>FIRST NAME</Form.Label>
-          <input
-            type="text"
-            value={firstName}
-            name="firstName"
-            onChange={(e) => {
-              setFirstName(e.target.value);
-            }}
-            className="form-control"
-            placeholder="First Name"
-          />
+    <div onSubmit={handleSubmit} className="w-75">
+      <Form>
+        {/* <Form.Label>FIRST NAME</Form.Label>
+        <input
+          type="text"
+          value={firstName}
+          name="firstName"
+          
+          className="form-control"
+          placeholder="First Name"
+        /> */}
+
+        <Row>
+            <Col>
+            <Form.Control placeholder="First name" onChange={(e) => {
+            setFirstName(e.target.value);
+          }}/>
+            </Col>
+            <Col>
+            <Form.Control placeholder="Last name" onChange={(e) => {
+            setLastName(e.target.value);
+          }}/>
+            </Col>
+        </Row>
 
         {/* ***********HANDLE SUBMIT*********** */}
-        <Button variant="primary" value='submit' type="submit">
+        <Button variant="primary" value="submit" type="submit">
           Submit
         </Button>
       </Form>
