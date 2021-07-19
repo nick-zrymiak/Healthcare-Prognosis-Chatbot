d9d5a06b236ebe103d7143ce943d08e060666b1a
awshaf <ahmmadawshaf@gmail.com>
2021-07-20 04:47:33 +0600

    added gender radio button forms

--- client/src/components/UserDataRegister.js
+++ client/src/components/UserDataRegister.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
-import { Button, Form, Container, Row , Col} from "react-bootstrap";
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
 
 export default function UserDataRegister() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [gender, setGender] = useState("Male");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("clicked on submit");
     console.log(firstName);
-    console.log(lastName)
+    console.log(lastName);
+    console.log(gender);
   };
 
   return (
@@ -26,17 +28,56 @@ export default function UserDataRegister() {
         /> */}
 
         <Row>
-            <Col>
-            <Form.Control placeholder="First name" onChange={(e) => {
-            setFirstName(e.target.value);
-          }}/>
-            </Col>
-            <Col>
-            <Form.Control placeholder="Last name" onChange={(e) => {
-            setLastName(e.target.value);
-          }}/>
-            </Col>
+          <Col>
+            <Form.Control
+              placeholder="First name"
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+          </Col>
+          <Col>
+            <Form.Control
+              placeholder="Last name"
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+            />
+          </Col>
         </Row>
+
+        <Form.Group as={Row} className="mb-3">
+      <Form.Label as="legend" column sm={2}>
+        Gender
+      </Form.Label>
+      <Col sm={10}>
+        <Form.Check
+          type="radio"
+          label="Male"
+          value="Male"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios1"
+          onChange={e=>{setGender("Male")}}
+        //   checked="Male"
+        />
+        <Form.Check
+          type="radio"
+          label="Female"
+          value="Female"
+          name="formHorizontalRadios"
+          onChange={e=>{setGender("Female")}}
+          id="formHorizontalRadios2"
+        />
+        <Form.Check
+          type="radio"
+          label="Other"
+          name="formHorizontalRadios"
+          value="Other"
+          onChange={ e => {setGender("Other")} }
+          id="formHorizontalRadios3"
+        />
+      </Col>
+    </Form.Group>
 
         {/* ***********HANDLE SUBMIT*********** */}
         <Button variant="primary" value="submit" type="submit">
