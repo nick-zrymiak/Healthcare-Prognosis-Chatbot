54c52b7dc882208ab77d1d7f4d8b1155003b290a
awshaf <ahmmadawshaf@gmail.com>
2021-07-25 23:02:30 +0600

    added main heartBeat animation in landing page

--- client/src/components/IntroSection.js
+++ client/src/components/IntroSection.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import introImage from '../images/intro-image.png';
+import React, { Component } from "react";
+import introImage from "../images/intro-image.png";
+import LandingHeartBeatAnim from "./LandingHeartBeatAnim";
 
 export default class IntroSection extends Component {
-    render() {
-        return ( 
-            <section className="introSection">
-                <div className="textSubSection">
-                    <h3>Getting Started</h3>
-                    <h5>
-                        Get Result with just 3 steps:
-                        <ol>
-                            <li>Create an Account with Us</li>
-                            <li>Upload Relavent Data</li>
-                            <li>Choose an Analysis Tool</li>
-                        </ol>
-                    </h5>
-                </div>
+  render() {
+    return (
+      <section className="introSection">
+        <div className="textSubSection">
+          <h5>Get Resutls in just 3 easy steps:</h5>
+          <h4>1. Create an Account with Us</h4>
+          <h4>2. Upload Relavent Data</h4>
+          <h4>3. Choose an Analysis Tool</h4>
+        </div>
 
-                <div className='imgSubSection'>
-                    <img src={introImage} width='280px' alt='brain-intro-img'/>
-                </div>
-            </section>
-        )
-    } 
+        <div className="imgSubSection">
+          {/* <img src={introImage} width='280px' alt='brain-intro-img'/> */}
+          <LandingHeartBeatAnim />
+        </div>
+      </section>
+    );
+  }
 }
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -12,6 +12,7 @@ export default function LandingPage() {
                 <IntroSection/>
                 <StatsRow/>
                 <ArticleAlpha/>
+                
 
             </div>          
         </div>
