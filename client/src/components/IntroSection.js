05efd6804a4cf8bf9627167cb44bef672aad6b1c
awshaf <ahmmadawshaf@gmail.com>
2021-07-28 02:14:09 +0600

    added tilt to landingPage heart

--- client/src/components/IntroSection.js
+++ client/src/components/IntroSection.js
@@ -16,9 +16,45 @@ export default class IntroSection extends Component {
         </div>
 
         <div className="imgSubSection">
-          <LandingHeartBeatAnim />
+          <Tilt
+            style={{
+              height: "400px",
+              backgroundColor: "rgba(160, 17, 17, 0.2)",
+              borderRadius: "50%",
+            }}
+            options={{ 
+              "max-glare": 23232, 
+              max: 360, 
+              scale: 0.1,
+              perspective: 2323,
+              transition:        false,
+              glare:             true, 
+              easing:            "cubic-bezier(.03,.98,.52,.99)"
+            }}
+          >
+            <LandingHeartBeatAnim />
+          </Tilt>
         </div>
       </section>
     );
   }
 }
+
+/*
+{
+    reverse:           false,  // reverse the tilt direction
+    max:               35,     // max tilt rotation (degrees)
+    perspective:       1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:             1,      // 2 = 200%, 1.5 = 150%, etc..
+    speed:             300,    // Speed of the enter/exit transition
+    transition:        true,   // Set a transition on enter/exit.
+    axis:              null,   // What axis should be disabled. Can be X or Y.
+    reset:             true    // If the tilt effect has to be reset on exit.
+    easing:            "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    glare:             false   // if it should have a "glare" effect
+    "max-glare":       1,      // the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
+    "glare-prerender": false   // false = VanillaTilt creates the glare elements for you, otherwise
+                               // you need to add .js-tilt-glare>.js-tilt-glare-inner by yourself
+}
+
+*/
