d8f0f77582037975b3e64e2ef99bee4a2d32415c
awshaf <ahmmadawshaf@gmail.com>
2021-08-13 06:21:39 +0600

    runChat function called

--- backend/server/heartapp/views.py
+++ backend/server/heartapp/views.py
@@ -11,3 +11,19 @@ from django.http import HttpResponse
 def index(request):
    response = User.objects.all()
    return HttpResponse(response)
+
+
+def runChat(request):
+   # print [1,2], '\n', [3,4]
+   # [1,2]
+   # [3,4]
+   response = 'runchat function ran'
+   return HttpResponse(response)
+
+# In Javascript:
+
+# console.log([1,2],'\n',[3,4])
+# prints
+
+# [1,2] '\n' [3,4
+   # var postreponse = User.objects.all()
