3e05ec58513f97818927ed2de57482da9bd81c29
nzrymiak <nzrymiak@sfu.ca>
2021-08-17 17:43:39 -0700

    feat: document prognosis code

--- backend/Prognosis/conversation.py
+++ backend/Prognosis/conversation.py
@@ -17,12 +17,7 @@ from gensim.similarities import SoftCosineSimilarity
 warnings.filterwarnings('ignore')
 
 def preprocess(text):
-    try:
-        stopwords = set(nltk.corpus.stopwords.words('english'))
-    except LookupError:
-        nltk.download('stopwords')
-        stopwords = set(nltk.corpus.stopwords.words('english'))
-    
+    stopwords = set(nltk.corpus.stopwords.words('english'))
     return [token for token in simple_preprocess(text, min_len=0, max_len=float('inf')) if token not in stopwords]
 
 def clean_symptoms(raw_symptoms, symptoms):
@@ -32,12 +27,18 @@ def clean_symptoms(raw_symptoms, symptoms):
         rs = rs.replace(' (typhos)', '')
         symptoms.append(rs)
 
-
 def get_doc_similarity_scores(user_msg, corpus, glove):
     similarity_index = WordEmbeddingSimilarityIndex(glove)
+
+    # build the term dictionary, TF-idf model
+    # the search query must be in the dictionary as well in case the terms do not overlap with the documents (we still want similarity of meaning)
     dictionary = Dictionary(corpus + [user_msg])
     tfidf = TfidfModel(dictionary=dictionary)
+
+    # create the term similarity matrix
     similarity_matrix = SparseTermSimilarityMatrix(similarity_index, dictionary, tfidf, nonzero_limit=None)
+
+    # compute Soft Cosine Measure between the user message and the symptoms
     user_msg_tf = tfidf[dictionary.doc2bow(user_msg)]
     index = SoftCosineSimilarity(
         tfidf[[dictionary.doc2bow(text) for text in corpus]], 
@@ -45,41 +46,63 @@ def get_doc_similarity_scores(user_msg, corpus, glove):
     doc_similarity_scores = index[user_msg_tf]
     return doc_similarity_scores, dictionary, similarity_matrix
 
+def filter_unlikely_symptoms(raw_symptoms, symptoms, doc_similarity_scores):
+    sorted_indexes = np.argsort(doc_similarity_scores)[:-1]
+    symptoms = np.zeros(len(raw_symptoms))
+    for i in sorted_indexes:
+        SIMILARITY_THRESH = .6
+        if doc_similarity_scores[i] > SIMILARITY_THRESH:
+            symptoms[i] = 1
+    
+    symptoms = [symptoms]
+    return symptoms
+
+def filter_prognoses(causes, cause_probabilities, probable_causes, bot_msg):
+    for cause in probable_causes:
+        prob = cause_probabilities[cause]
+        SIMILARITY_THRESH = .05
+        if prob > SIMILARITY_THRESH:
+            bot_msg += '\n\t' + causes[cause] + ': ' + str(prob * 100) + '%'
+    
+    return bot_msg
+
 def chat(user_msg):
+    # import and download stopwords from NLTK
+    try:
+        stopwords = set(nltk.corpus.stopwords.words('english'))
+    except LookupError:
+        nltk.download('stopwords')
+
     train = pd.read_csv('./data/Training.csv')
     raw_symptoms = train.drop('prognosis', axis=1)
     raw_symptoms = raw_symptoms.columns
     symptoms = []
-    
     clean_symptoms(raw_symptoms, symptoms)
     causes = train['prognosis'].unique()
+
     corpus = [preprocess(symptom) for symptom in symptoms]
     user_msg = preprocess(user_msg)
     
-    if 'glove' not in locals():
-        glove = api.load("glove-wiki-gigaword-50")
+    # download and/or load the GloVe word vector embeddings
+    if 'glove' not in locals(): # only load if not already in memory
+        glove = api.load('glove-wiki-gigaword-50')
         
     doc_similarity_scores, dictionary, similarity_matrix = get_doc_similarity_scores(user_msg, corpus, glove)
-    sorted_indexes = np.argsort(doc_similarity_scores)[::-1]
+    symptoms = filter_unlikely_symptoms(raw_symptoms, symptoms, doc_similarity_scores)
     
-    symptoms = np.zeros(len(raw_symptoms))
-    for i in sorted_indexes:
-        if doc_similarity_scores[i] > .5:
-          symptoms[i] = 1
-    symptoms = [symptoms]
-    
-    prognosis_model = keras.models.load_model('prognosis_model.h5')
-    cause_probabilities = prognosis_model.predict(np.array(symptoms))
-    cause_probabilities = cause_probabilities[0]
-    
-    probable_causes = cause_probabilities.argsort()[::-1]
+    min_one_symptom_recognized = np.isin(1, symptoms[0])
     bot_msg = 'The following may be cause(s) of your symptom(s) with corresponding chances: '
-    
-    for cause in probable_causes:
-      prob = cause_probabilities[cause]
-      
-      if prob > .05:
-        bot_msg += '\n\t' + causes[cause] + ': ' + str(prob*100) + '%'
+    if min_one_symptom_recognized:    
+        prognosis_model = keras.models.load_model('prognosis_model.h5')
+        cause_probabilities = prognosis_model.predict(np.array(symptoms))
+        cause_probabilities = cause_probabilities[0]
+        probable_causes = cause_probabilities.argsort()[::-1] # sort in descending order
+        bot_msg = filter_prognoses(causes, cause_probabilities, probable_causes, bot_msg)
         
-    bot_msg = 'I didn\'t get that, try again.' if bot_msg == '' else bot_msg
+        low_confidence_for_all_causes = bot_msg == 'The following may be cause(s) of your symptom(s) with corresponding chances: '
+        if low_confidence_for_all_causes:
+            bot_msg = 'I don\'t have enough information to give an accurate prediction with the symptoms you stated.'
+    else:
+        bot_msg = 'I didn\'t get that, try again.' 
+    
     return bot_msg
